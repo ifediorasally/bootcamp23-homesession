e07197e67c7ce9fd809e3cc10c4ba65277ea0c7a
unknown <ifediorasally@gmail.com>
2017-06-06 12:23:27 +0100

    this is my first commit

---
+++ app/library.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+
+/* Find the prime numbers */ 
+functiongetPrimes(n)  { 
+
+    varlist = [];
+    for (var i = 2; i! = n;++i)
+    if (isPrime (i))list.push(i)
+    return varlist;
+},
+}
